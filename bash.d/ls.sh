8722f6ff2d76e71c38867b335c491919ee253b4d
Jared Crapo <jared@kotfu.net>
2017-07-05 23:09:17 -0600

    Fix bug in l (as in lowercase letter ell) function

--- bash.d/ls.sh
+++ bash.d/ls.sh
@@ -2,21 +2,28 @@
 # nice functions for listing files
 
 function l () {
-    local LSCNT=$(( $(ls $@ | wc -l) + 0 ))
-    if [ $LSCNT -ge $LINES ]; then
-        ls -ld "$@" | grep -v '^total' | less -e
+    if [ $# -eq 0 ]; then
+        local LSCMD="ls -ld *"
+        local LSCNT=$(( $( $LSCMD | wc -l) + 0 ))
     else
-        ls -ld "$@" | grep -v '^total'
+        local LSCMD="ls -ld $@"
+        local LSCNT=$(( $( $LSCMD | wc -l) + 0 ))
+    fi
+
+    if [ $LSCNT -ge $LINES ]; then
+        eval "$LSCMD | grep -v '^total' | less -e"
+    else
+        eval "$LSCMD | grep -v '^total'"
     fi
 }
 
 function la() {
     if [ $# -eq 0 ]; then
-        LSCMD="ls -ald .* *"
-        LSCNT=$(( $( $LSCMD | wc -l) + 0 ))
+        local LSCMD="ls -ald .* *"
+        local LSCNT=$(( $( $LSCMD | wc -l) + 0 ))
     else
-        LSCMD="ls -ald $@"
-        LSCNT=$(( $( $LSCMD | wc -l) + 0 ))
+        local LSCMD="ls -ald $@"
+        local LSCNT=$(( $( $LSCMD | wc -l) + 0 ))
     fi
 
     if [ $LSCNT -ge $LINES ]; then
