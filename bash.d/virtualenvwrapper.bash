83f9e6d46fcb6aa28eb4942801ad07fb9944dbc6
Jared Crapo <jared@kotfu.net>
2017-08-11 11:45:24 -0600

    disable virtualenv prompt

--- bash.d/virtualenvwrapper.bash
+++ bash.d/virtualenvwrapper.bash
@@ -3,6 +3,7 @@
 WRAPPER=$(which virtualenvwrapper.sh)
 if [ $? == 0 ]; then
     VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+    VIRTUAL_ENV_DISABLE_PROMPT=yes
     WORKON_HOME=$HOME/.virtualenvs
     PROJECT_HOME=$HOME/src
     source $WRAPPER
