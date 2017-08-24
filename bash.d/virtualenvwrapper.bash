cb82f5546950952d90f7f463cf921d04fe0d7c03
Jared Crapo <jared@kotfu.net>
2017-08-24 13:14:57 -0600

    disable virtualenvwrapper, replaced by pyenv

--- bash.d/virtualenvwrapper.bash
+++ bash.d/virtualenvwrapper.bash
@@ -1,10 +1,10 @@
 #
 # if virtualenvwrapper is installed, activate it
-WRAPPER=$(which virtualenvwrapper.sh)
-if [ $? == 0 ]; then
-    VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
-    VIRTUAL_ENV_DISABLE_PROMPT=yes
-    WORKON_HOME=$HOME/.virtualenvs
-    PROJECT_HOME=$HOME/src
-    source $WRAPPER
-fi
+#WRAPPER=$(which virtualenvwrapper.sh)
+#if [ $? == 0 ]; then
+#    VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python3
+#    VIRTUAL_ENV_DISABLE_PROMPT=yes
+#    WORKON_HOME=$HOME/.virtualenvs
+#    PROJECT_HOME=$HOME/src
+#    source $WRAPPER
+#fi
