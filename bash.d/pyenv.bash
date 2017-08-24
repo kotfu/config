398cbbac2694b9362bbebe704030b97836d05076
Jared Crapo <jared@kotfu.net>
2017-08-24 13:14:32 -0600

    Tweak pyenv to not mess with the shell prompt

--- bash.d/pyenv.bash
+++ bash.d/pyenv.bash
@@ -3,4 +3,9 @@
 PYENV=$(which pyenv)
 if [ $? == 0 ]; then
     eval "$(pyenv init -)"
+    VENV=$(pyenv virtualenv-init -)
+    if [ $? == 0 ]; then
+        eval "$(pyenv virtualenv-init -)"
+        export VIRTUAL_ENV_DISABLE_PROMPT=yes
+    fi
 fi
