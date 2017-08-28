fc6852ba2b353d11672e8fa9da3a605bcbd619c0
Jared Crapo <jared@kotfu.net>
2017-08-27 18:18:18 -0600

    Redirect error message if virtualenv-init is not installed

--- bash.d/pyenv.bash
+++ bash.d/pyenv.bash
@@ -4,7 +4,7 @@
 PYENV=$(which pyenv)
 if [ $? == 0 ]; then
     eval "$(pyenv init -)"
-    VENV=$(pyenv virtualenv-init -)
+    VENV=$(pyenv virtualenv-init - 2>/dev/null)
     if [ $? == 0 ]; then
         eval "$(pyenv virtualenv-init -)"
         export VIRTUAL_ENV_DISABLE_PROMPT=yes
