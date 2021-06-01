a17ad8cba03f53162ce2a6d96094d725d54bb49d
kotfu <kotfu@kotfu.net>
2021-05-31 20:45:58 -0600

    Fix bad exit 0 error

--- bashrc.d/clipboard.bash
+++ bashrc.d/clipboard.bash
@@ -3,60 +3,59 @@
 #
 # copy darn near anything to the clipboard in macos
 
-if [[ ! $OSTYPE =~ darwin* ]]; then
-    exit 0
+if [[ $OSTYPE =~ darwin* ]]; then
+
+    clipboard() {
+        if [[ $1 == "-h" || $1 == "-H" || $1 == "--help" ]]; then
+            echo "Intelligently copies command results, text file, or raw text to"
+            echo "OS X clipboard"
+            echo
+            echo "Usage: clipboard [command or text]"
+            echo "  or pipe a command: [command] | clipboard"
+            return
+        fi
+
+        local output
+        local res=false
+        local msg=""
+
+        if [[ $# == 0 ]]; then
+            output=$(cat)
+            msg="Input copied to clipboard"
+            res=true
+        else
+            local cmd=""
+            for arg in $@; do
+                cmd+="\"$(echo -en $arg|sed -E 's/"/\\"/g')\" "
+            done
+            output=$(eval "$cmd" 2> /dev/null)
+            if [[ $? == 0 ]]; then
+                msg="Results of command are in the clipboard"
+                res=true
+            else
+                if [[ -f $1 ]]; then
+                    output=""
+                    for arg in $@; do
+                        if [[ -f $arg ]]; then
+                            type=`file "$arg"|grep -c text`
+                            if [ $type -gt 0 ]; then
+                                output+=$(cat $arg)
+                                msg+="Contents of $arg are in the clipboard."
+                                res=true
+                            else
+                                msg+="File \"$arg\" is not plain text."
+                            fi
+                        fi
+                    done
+                else
+                    output=$@
+                    msg="Text copied to clipboard"
+                    res=true
+                fi
+            fi
+        fi
+
+        $res && echo -ne "$output" | pbcopy -Prefer txt
+        echo -e "$msg"
+    }
 fi
-
-clipboard() {
-	if [[ $1 == "-h" || $1 == "-H" || $1 == "--help" ]]; then
-		echo "Intelligently copies command results, text file, or raw text to"
-		echo "OS X clipboard"
-		echo
-		echo "Usage: clipboard [command or text]"
-		echo "  or pipe a command: [command] | clipboard"
-		return
-	fi
-
-	local output
-	local res=false
-	local msg=""
-
-	if [[ $# == 0 ]]; then
-		output=$(cat)
-		msg="Input copied to clipboard"
-		res=true
-	else
-		local cmd=""
-		for arg in $@; do
-			cmd+="\"$(echo -en $arg|sed -E 's/"/\\"/g')\" "
-		done
-		output=$(eval "$cmd" 2> /dev/null)
-		if [[ $? == 0 ]]; then
-			msg="Results of command are in the clipboard"
-			res=true
-		else
-			if [[ -f $1 ]]; then
-				output=""
-				for arg in $@; do
-					if [[ -f $arg ]]; then
-						type=`file "$arg"|grep -c text`
-						if [ $type -gt 0 ]; then
-							output+=$(cat $arg)
-							msg+="Contents of $arg are in the clipboard."
-							res=true
-						else
-							msg+="File \"$arg\" is not plain text."
-						fi
-					fi
-				done
-			else
-				output=$@
-				msg="Text copied to clipboard"
-				res=true
-			fi
-		fi
-	fi
-
-	$res && echo -ne "$output" | pbcopy -Prefer txt
-	echo -e "$msg"
-}
