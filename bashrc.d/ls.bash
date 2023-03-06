f8dfad7f092e60713d239b75625da1351a49ef85
kotfu <kotfu@kotfu.net>
2023-03-06 13:44:22 -0600

    Cleanups and big fixes
    - `less —quit-at-one screen` didn’t exist when I initially wrote this 20 years ago, so I was counting the lines. Now that it exists, these functions can be streamlined
    - fix a bug that created an `la` error if you did it in a directory with no dotfiles

--- bashrc.d/ls.bash
+++ bashrc.d/ls.bash
@@ -11,40 +11,24 @@ function l () {
             local LSCMD="ls -lhd $@"
         fi
     fi
-    local LSCNT=$( $LSCMD 2>/dev/null | wc -l )
-    #LSCNT=$(( $LSCNT + 0 ))
 
-    if [ $LSCNT -ge $LINES ]; then
-        eval "$LSCMD | grep -v '^total' | less -e"
-    else
-        eval "$LSCMD | grep -v '^total'"
-    fi
+	local LESSCMD="less --quit-at-eof --quit-if-one-screen"
+    eval "$LSCMD --color=always | grep -v '^total' | $LESSCMD"
 }
 
 function la() {
     if [ $# -eq 0 ]; then
-        # see if we have an empty directory
-        local LSCNT=$( ls -lhd * 2>/dev/null | wc -l )
-        if [ $LSCNT -eq 0 ]; then
-            # only show the two dotfiles, avoiding an error for ls *
-            local LSCMD="ls -lhd .*"
-        else
-            # show everything
-            local LSCMD="( ls -lhd .* && ls -lhd * )"
-        fi
+        local LSCMD="ls -alh --color=always"
     else
         if [[ "$@" =~ ^- ]]; then
-            local LSCMD="ls -Alh $@"
+			# command line options include more options for ls
+            local LSCMD="ls -Alh --color=always $@"
         else
-            local LSCMD="ls -Alhd $@"
+			# command line options name a file or directory, don't show the contents
+            local LSCMD="ls -Alhd --color=always $@"
         fi
     fi
-    local LSCNT=$(eval $LSCMD 2>/dev/null | wc -l)
-    #LSCNT=$(( $LSCNT + 0 ))
 
-    if [ $LSCNT -ge $LINES ]; then
-        eval "$LSCMD | grep -v '^total' | less -e"
-    else
-        eval "$LSCMD | grep -v '^total'"
-    fi
+	local LESSCMD="less --quit-at-eof --quit-if-one-screen"
+    eval "$LSCMD | grep -v '^total' | $LESSCMD"
 }
