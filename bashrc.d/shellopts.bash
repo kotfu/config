2278df787fd8bdd4bffd75e55c37b84f4e09d6d2
kotfu <kotfu@kotfu.net>
2023-03-07 00:47:11 -0600

    Add GLOBIGNORE for . and ..

--- bashrc.d/shellopts.bash
+++ bashrc.d/shellopts.bash
@@ -6,9 +6,13 @@ export IGNOREEOF=1
 # make echo do the escape sequences by default
 shopt -s xpg_echo
 shopt -s extglob cdspell checkwinsize no_empty_cmd_completion
+# don't glob . and .. with .*
+export GLOBIGNORE='.:..'
+
 # update $LINES and $COLUMNS
 shopt -s checkwinsize
 
+# set the prompt
 if [ $(whoami) == 'root' ]; then
     PS1="\h:\w# "
 else
