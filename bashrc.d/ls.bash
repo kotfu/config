7cd93a10c024dc1ca073a03a30280fe92919ea83
kotfu <kotfu@kotfu.net>
2023-03-11 21:15:26 -0700

    use gls on macos

--- bashrc.d/ls.bash
+++ bashrc.d/ls.bash
@@ -2,13 +2,20 @@
 # nice functions for listing files
 
 function l () {
+	local LS="ls"
+	if [[ $OSTYPE =~ darwin* ]]; then
+		# have to `brew install coreutils` to get this
+		# we use gls because it's dircolors are more flexible than the
+		# ls that comes with macos
+		LS="gls"
+	fi
     if [ $# -eq 0 ]; then
-        local LSCMD="ls -lh"
+        local LSCMD="$LS -lh"
     else
         if [[ "$@" =~ ^- ]]; then
-            local LSCMD="ls -lh $@"
+            local LSCMD="$LS -lh $@"
         else
-            local LSCMD="ls -lhd $@"
+            local LSCMD="$LS -lhd $@"
         fi
     fi
 
@@ -17,19 +24,27 @@ function l () {
 }
 
 function la() {
+	local LS="ls"
+	if [[ $OSTYPE =~ darwin* ]]; then
+		# have to `brew install coreutils` to get this
+		# we use gls because it's dircolors are more flexible than the
+		# ls that comes with macos
+		LS="gls"
+	fi
     if [ $# -eq 0 ]; then
 		# on macos dotfiles are always shown first when you do:
         # local LSCMD="ls -alh --color=always"
 		# but on linux, they are not
 		# this incantation always shows dotfiles first
-		local LSCMD="(ls -lhd --color=always .* 2>/dev/null ; ls -lh --color=always)"
+		#local LSCMD="($LS -lhd --color=always .* 2>/dev/null ; ls -lh --color=always)"
+		local LSCMD="$LS -Alh --color=always"
     else
         if [[ "$@" =~ ^- ]]; then
 			# command line options include more options for ls
-            local LSCMD="ls -Alh --color=always $@"
+            local LSCMD="$LS -Alh --color=always $@"
         else
 			# command line options name a file or directory, don't show the contents
-            local LSCMD="ls -Alhd --color=always $@"
+            local LSCMD="$LS -Alhd --color=always $@"
         fi
     fi
 
