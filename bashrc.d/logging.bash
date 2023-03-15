e3634e0991753f41422702fcfa8a3b403974fa23
kotfu <kotfu@kotfu.net>
2023-03-15 17:39:23 -0600

    Reformat comments

--- bashrc.d/logging.bash
+++ bashrc.d/logging.bash
@@ -1,6 +1,7 @@
 #
-# logr should be loaded by profile, but in some scenarios (i.e. scp) profile doesn't
-# get run. This check is logr is loaded, and if not, loads it.
+# logr should be loaded by profile, but in some scenarios
+# (i.e. scp) profile doesn't get run. This checks if logr is
+# loaded, and if not, loads it.
 
 type logr >/dev/null 2>&1
 if [ $? -eq 1 ]; then
