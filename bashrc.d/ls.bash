8587380172448f2b3729e635623f01dfe411af8c
kotfu <kotfu@kotfu.net>
2023-03-15 23:30:26 -0600

    try another fix for openbsd

--- bashrc.d/ls.bash
+++ bashrc.d/ls.bash
@@ -2,18 +2,18 @@
 # nice functions for listing files
 
 function l () {
-	local LS="ls"
-	if [[ $OSTYPE =~ darwin* ]]; then
-		# have to `brew install coreutils` to get this
-		# we use gls because it's dircolors are more flexible than the
-		# ls that comes with macos
-		LS="gls"
-	fi
-	if [[ $OSTYPE =~ openbsd* ]]; then
-		# have to 'pkg_add coreutils' to get this
-		# openbsd ls doesn't have color support
-		LS="gls"
-	fi
+    local LS="ls"
+    if [[ $OSTYPE =~ darwin* ]]; then
+        # have to `brew install coreutils` to get this
+        # we use gls because it's dircolors are more flexible than the
+        # ls that comes with macos
+        LS="gls"
+    fi
+    if [[ $OSTYPE =~ openbsd* ]]; then
+        # have to 'pkg_add coreutils' to get this
+        # openbsd ls doesn't have color support
+        LS="gls"
+    fi
     if [ $# -eq 0 ]; then
         local LSCMD="$LS -lh"
     else
@@ -24,54 +24,54 @@ function l () {
         fi
     fi
 
-    local LESSCMD="| less --quit-at-eof --quit-if-one-screen --tilde --no-init"
+    local LESSCMD="less --quit-at-eof --quit-if-one-screen --tilde --no-init --chop-long-lines"
     # openbsd less draws horribly if --quit-if-one-screen and if the
     # output is less than one screen long
+    # adding --clear-screen fixes it, but it breaks output on other
+    # operating systems, so we only add it if it's openbsd
     if [[ $OSTYPE =~ openbsd* ]]; then
-        if [[ $($LSCMD | wc -l) -lt $LINES ]]; then
-            LESSCMD=""
-        fi
-    fi	
-    eval "$LSCMD --color=always | grep -v '^total' $LESSCMD"
+        LESSCMD="$LESSCMD --clear-screen"
+    fi  
+    eval "$LSCMD --color=always | grep -v '^total' | $LESSCMD"
 }
 
 function la() {
-	local LS="ls"
-	if [[ $OSTYPE =~ darwin* ]]; then
-		# have to `brew install coreutils` to get this
-		# we use gls because it's dircolors are more flexible than the
-		# ls that comes with macos
-		LS="gls"
-	fi
-	if [[ $OSTYPE =~ openbsd* ]]; then
-		# have to 'pkg_add coreutils' to get this
-		# openbsd ls doesn't have color support
-		LS="gls"
-	fi
+    local LS="ls"
+    if [[ $OSTYPE =~ darwin* ]]; then
+        # have to `brew install coreutils` to get this
+        # we use gls because it's dircolors are more flexible than the
+        # ls that comes with macos
+        LS="gls"
+    fi
+    if [[ $OSTYPE =~ openbsd* ]]; then
+        # have to 'pkg_add coreutils' to get this
+        # openbsd ls doesn't have color support
+        LS="gls"
+    fi
     if [ $# -eq 0 ]; then
-		# on macos dotfiles are always shown first when you do:
+        # on macos dotfiles are always shown first when you do:
         # local LSCMD="ls -alh --color=always"
-		# but on linux, they are not
-		# this incantation always shows dotfiles first
-		#local LSCMD="($LS -lhd --color=always .* 2>/dev/null ; ls -lh --color=always)"
-		local LSCMD="$LS -Alh --color=always"
+        # but on linux, they are not
+        # this incantation always shows dotfiles first
+        #local LSCMD="($LS -lhd --color=always .* 2>/dev/null ; ls -lh --color=always)"
+        local LSCMD="$LS -Alh --color=always"
     else
         if [[ "$@" =~ ^- ]]; then
-			# command line options include more options for ls
+            # command line options include more options for ls
             local LSCMD="$LS -Alh --color=always $@"
         else
-			# command line options name a file or directory, don't show the contents
+            # command line options name a file or directory, don't show the contents
             local LSCMD="$LS -Alhd --color=always $@"
         fi
     fi
 
-    local LESSCMD="| less --quit-at-eof --quit-if-one-screen --tilde --no-init"
+    local LESSCMD="less --quit-at-eof --quit-if-one-screen --tilde --no-init --chop-long-lines"
     # openbsd less draws horribly if --quit-if-one-screen and if the
     # output is less than one screen long
+    # adding --clear-screen fixes it, but it breaks output on other
+    # operating systems, so we only add it if it's openbsd
     if [[ $OSTYPE =~ openbsd* ]]; then
-        if [[ $($LSCMD | wc -l) -lt $LINES ]]; then
-            LESSCMD=""
-        fi
-    fi	
-    eval "$LSCMD | grep -v '^total' $LESSCMD"
+        LESSCMD="$LESSCMD --clear-screen"
+    fi  
+    eval "$LSCMD | grep -v '^total' | $LESSCMD"
 }
