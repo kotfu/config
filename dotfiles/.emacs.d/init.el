8ebca65980d09f9cf1bb1f256759849537ad515d
kotfu <kotfu@kotfu.net>
2023-03-22 16:18:33 -0600

    Add .emacs.d

---
+++ dotfiles/.emacs.d/init.el
@@ -0,0 +1,7 @@
+;; general theme settings
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+
+;; dracula theme related settings
+
+(setq dracula-enlarge-headings nil)
